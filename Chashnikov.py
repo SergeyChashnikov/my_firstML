f023f6b3314c9b0d9f7dd061c7796d9327a33f05
SergeyChashnikov <149199873+SergeyChashnikov@users.noreply.github.com>
2023-11-29 18:55:04 +0300

    Update Chashnikov.py

--- Chashnikov.py
+++ Chashnikov.py
@@ -1,3 +1,4 @@
+# Импортируем библиотеки
 import io
 import streamlit as st
 import transformers
@@ -6,9 +7,9 @@ from transformers import pipeline
 st.title(' Переводчик с английского на русский ')
 
 res = st.text_input('Пожалуйста введите текст')
-
-en_ru_translator = pipeline("translation_en_to_ru", 'Helsinki-NLP/opus-mt-en-ru')
-trans = en_ru_translator(res)
-st.write('Перевод')
-for i in trans:
-    st.write(i['translation_text'])
+if st.button('Перевести'):
+    en_ru_translator = pipeline("translation_en_to_ru", 'Helsinki-NLP/opus-mt-en-ru')
+    trans = en_ru_translator(res)
+    st.write('Перевод')
+    for i in trans:
+        st.write(i['translation_text'])
