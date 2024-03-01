174aca2ff91d3fd0bb957d68bb9e2554aa71f5b9
SergeyChashnikov <chashserq@gmail.com>
2024-03-01 21:50:15 +0300

    edit test_Chashnikov.py

--- test_Chashnikov.py
+++ test_Chashnikov.py
@@ -1,19 +1,18 @@
 from streamlit.testing.v1 import AppTest
 
+
 def test_text_input():
-     
-    at = AppTest.from_file('Chashnikov.py')
-    
+
+    at = AppTest.from_file("Chashnikov.py")
+
     at.run(timeout=30)
 
-
-    #print(at.main)
-    text = 'Good evening my little friend'
+    # print(at.main)
+    text = "Good evening my little friend"
     at.text_input[0].set_value(text).run()
     at.button[0].click().run(timeout=60)
 
-    #print(at.main)
-    #print(at.markdown[1].value)
+    # print(at.main)
+    # print(at.markdown[1].value)
 
-    assert at.markdown[1].value == 'Добрый вечер, мой маленький друг.'
-    
+    assert at.markdown[1].value == "Добрый вечер, мой маленький друг."
