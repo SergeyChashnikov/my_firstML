f4acca453ae376aec8a7c9fe8287bda119675de8
SergeyChashnikov <chashserq@gmail.com>
2024-03-17 01:57:09 +0300

    mod test

--- test_Chashnikov.py
+++ test_Chashnikov.py
@@ -7,14 +7,14 @@ def test_text_input():
 
     at.run(timeout=30)
 
-    #print(at.main)
+    print(at.main)
     text = "Good evening my little friend"
     at.text_input[0].set_value(text).run()
     at.button[0].click().run(timeout=60)
 
-    #print(at.main)
-    #print(at.markdown[0].value)
-    print(at.markdown())
+    print(at.main)
+    print(at.markdown[0].value)
+    print(at.markdown[1])
 
     #assert at.markdown[1].value == "Добрый вечер, мой маленький друг."
 
