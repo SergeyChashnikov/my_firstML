343c07ef09130539869a98941f36f73ae78c673b
SergeyChashnikov <149199873+SergeyChashnikov@users.noreply.github.com>
2024-03-19 01:29:14 +0300

    Update Chashnikov.py
    with PEP8

--- Chashnikov.py
+++ Chashnikov.py
@@ -2,13 +2,11 @@
 import streamlit as st
 from transformers import pipeline
 
-
 st.title(" Переводчик с английского на русский ")
-
 res = st.text_input("Пожалуйста введите текст")
 if st.button("Перевести"):
     en_ru_translator = pipeline("translation",
-                                model="Helsinki-NLP/opus-mt-en-ru")
+                                 model="Helsinki-NLP/opus-mt-en-ru")
     trans = en_ru_translator(res)
     st.write("Перевод")
     st.text(trans[0]["translation_text"])
