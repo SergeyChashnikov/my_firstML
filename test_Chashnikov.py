6dc7773d55c798f5da580356afc4394dfd444ea4
SergeyChashnikov <chashserq@gmail.com>
2024-03-18 19:34:40 +0300

    edit test

--- test_Chashnikov.py
+++ test_Chashnikov.py
@@ -15,7 +15,8 @@ def test_text_input():
     #print(at.main.children[1])
     print(at.main)
     print(at.main.children[3])
-    #print(str(at.text[0].value))
+    print(at.main.children[3].value)
+    print(at.text)
     #print(at.markdown[1].value)
     print(at.main)
 
