9ed86823777461dc7299fcf2975326d8f3adeeca
SergeyChashnikov <chashserq@gmail.com>
2024-03-18 19:11:08 +0300

    del perevod

--- Chashnikov.py
+++ Chashnikov.py
@@ -9,5 +9,5 @@ if st.button("Перевести"):
     en_ru_translator = pipeline("translation",
                                 model = "Helsinki-NLP/opus-mt-en-ru")
     trans = en_ru_translator(res)
-    st.write("Перевод")
+    #st.write("Перевод")
     st.text(trans[0]["translation_text"])
