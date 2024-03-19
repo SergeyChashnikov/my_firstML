3e758275e8f6e29bb19fd85b48e3a8f065413192
SergeyChashnikov <chashserq@gmail.com>
2024-03-19 10:09:54 +0300

    (PEP8) del empty str, add comment

--- test_Chashnikov.py
+++ test_Chashnikov.py
@@ -1,28 +1,25 @@
 from streamlit.testing.v1 import AppTest
 
 
+# Тест для проверки заголовка
 def test_title():
 
     at = AppTest.from_file("Chashnikov.py")
-
     at.run(timeout=30)
-
     # print(at.title[0].value)
 
     assert at.title[0].value == "Переводчик с английского на русский"
 
 
+# Тест для проверки перевода
 def test_text_input():
 
     at = AppTest.from_file("Chashnikov.py")
-
     at.run(timeout=30)
-
     # print(at.main)
     text = "Good evening my little friend"
     at.text_input[0].set_value(text).run()
     at.button[0].click().run(timeout=60)
-
     # print(at.main)
     # print(at.markdown[1].value)
 
