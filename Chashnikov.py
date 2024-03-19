343eba91099477b3cc25760708491e1f78a398d0
SergeyChashnikov <chashserq@gmail.com>
2024-03-19 08:45:12 +0300

    update Chashnikov

--- Chashnikov.py
+++ Chashnikov.py
@@ -6,7 +6,8 @@ st.title(" Переводчик с английского на русский ")
 
 res = st.text_input("Пожалуйста введите текст")
 if st.button("Перевести"):
-    en_ru_translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
+    en_ru_translator = pipeline("translation",
+                                model="Helsinki-NLP/opus-mt-en-ru")
     trans = en_ru_translator(res)
     st.write("Перевод")
     st.text(trans[0]["translation_text"])
